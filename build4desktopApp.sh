ce21b0b20b6aa0562fc6c74cd730e33f9afd950b
Tobias Bachmann <t.bachmann@em.uni-frankfurt.de>
2023-01-16 18:48:16 +0100

    update build to steffani

--- build4desktopApp.sh
+++ build4desktopApp.sh
@@ -4,7 +4,7 @@ rm -R ./build4desktopApp
 
 mkdir ./build4desktopApp
 
-#cp -R ./app ./build4desktopApp
+cp -R ./app ./build4desktopApp
 cp -R ./data ./build4desktopApp
 cp -R ./ext-ux ./build4desktopApp
 cp -R ./extjs ./build4desktopApp
@@ -16,6 +16,6 @@ cp -R ./resources ./build4desktopApp
 cp -R ./scripts ./build4desktopApp
 cp -R ./*.xml ./build4desktopApp
 cp -R ./*.js ./build4desktopApp
-cp -R ./liav.html ./build4desktopApp
+cp -R ./edition-74338566.html ./build4desktopApp
 cp -R ./*.xql ./build4desktopApp
 cp -R ./*.xconf ./build4desktopApp
